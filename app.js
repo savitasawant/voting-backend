66ca4c9fa2d1e6f5c0b8707b1e31baff9a58e4b2
savita <savita.sawant@freightbro.com>
2020-08-02 19:51:56 +0530

    allow methods

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "referer-domain, Authorization, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
