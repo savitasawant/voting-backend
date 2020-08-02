a762945fdca23deacfe69c691294c75db3cefcfc
savita <savita.sawant@freightbro.com>
2020-08-02 19:41:59 +0530

    cors headers add

--- app.js
+++ app.js
@@ -15,15 +15,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT;
 
 // Enable all cors requests
-// app.use(cors());
+app.use(cors());
 app.use(function(req, res, next) {
-
   res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Headers", "*");
-
+  res.header("Access-Control-Allow-Headers", "referer-domain, Authorization, Origin, X-Requested-With, Content-Type, Accept");
   next();
-
 });
 
 
